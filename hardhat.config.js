dccde42fe00decccb12356609164a817f6cd7219
Femi Onakunle <femi_onakunle@hotmail.com>
2022-07-02 09:09:49 +0100

    Cosmetic changes
    Updated configuration, cosmetic changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,26 +1,28 @@
-require("dotenv").config()
+require("dotenv").config();
 
-require("@nomiclabs/hardhat-etherscan")
-require("@nomiclabs/hardhat-waffle")
-require("hardhat-gas-reporter")
-require("solidity-coverage")
-require("hardhat-deploy")
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("hardhat-deploy");
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-    //solidity: "0.8.8",
     solidity: {
         compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
     },
     defaultNetwork: "hardhat",
     networks: {
+        hardhat: {
+            chainId: 31337,
+        },
         rinkeby: {
             url: RINKEBY_RPC_URL || "",
             accounts: [PRIVATE_KEY],
@@ -47,4 +49,4 @@ module.exports = {
             default: 1,
         },
     },
-}
+};
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -7,15 +7,15 @@ const networkConfig = {
         name: "polygon",
         ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
     },
-}
+};
 
-const developmentChains = ["hardhat", "localhost"]
-const DECIMALS = 8
-const INITIAL_ANSWER = 2e11
+const developmentChains = ["hardhat", "localhost"];
+const DECIMALS = 8;
+const INITIAL_ANSWER = 2e11;
 
 module.exports = {
     networkConfig,
     developmentChains,
     DECIMALS,
     INITIAL_ANSWER,
-}
+};
